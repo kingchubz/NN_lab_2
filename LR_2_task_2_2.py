e50b30246bdd8442e2e82c6232a311f7572d4f18
anon <shinyhero777@gmail.com>
2024-03-27 23:33:49 +0200

    Initial commit

---
+++ LR_2_task_2_2.py
@@ -0,0 +1,102 @@
+# task_2_2
+
+def warn(*args, **kwargs):
+    pass
+import warnings
+warnings.warn = warn
+
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.metrics
+from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn import model_selection
+
+input_file = 'income_data.txt'
+
+X = []
+y = []
+count_class1 = 0
+count_class2 = 0
+max_datapoints = 25000
+
+with open(input_file, 'r') as f:
+    for line in f.readlines():
+        if count_class1 >= max_datapoints and count_class2 >= max_datapoints:
+            break
+
+        if '?' in line:
+            continue
+
+        data = line[:-1].split(', ')
+
+        if data[-1] == '<=50K' and count_class1 < max_datapoints:
+            X.append(data)
+            count_class1 += 1
+
+        if data[-1] == '>50K' and count_class2 < max_datapoints:
+            X.append(data)
+            count_class2 += 1
+X = np.array(X)
+
+label_encoder = []
+X_encoded = np.empty(X.shape)
+
+# Кодуємо символьні атрибути у числові
+for i,item in enumerate(X[0]):
+    if item.isdigit():
+        X_encoded[:, i] = X[:, i]
+    else:
+        label_encoder.append(preprocessing.LabelEncoder())
+        X_encoded[:, i] = label_encoder[-1].fit_transform(X[:,i])
+
+X = X_encoded[:, :-1].astype(int)
+y = X_encoded[:, -1].astype(int)
+
+# Розділяємо dataset на навчальний та тестовий набори
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=5)
+
+# Навчаємо класифікатор
+classifier = OneVsOneClassifier(SVC(kernel='rbf'))
+classifier.fit(X_train, y_train)
+
+y_test_pred = classifier.predict(X_test)
+
+# Обчислення F-міри для SVМ-класифікатора
+f1 = model_selection.cross_validate(classifier, X, y, scoring='f1_weighted', cv=3)
+print("F1 score: " + str(round(100*f1['test_score'].mean(), 2)) + "%")
+
+# Обчислення акуратність для SVМ-класифікатора
+accuracy = model_selection.cross_validate(classifier, X, y, scoring='accuracy', cv=3)
+print("Accuracy score: " + str(round(100*accuracy['test_score'].mean(), 2)) + "%")
+
+# Обчислення повноти для SVМ-класифікатора
+recall = model_selection.cross_validate(classifier, X, y, scoring='recall', cv=3)
+print("Recall score: " + str(round(100*recall['test_score'].mean(), 2)) + "%")
+
+# Обчислення точності для SVМ-класифікатора
+precision = model_selection.cross_validate(classifier, X, y, scoring='precision', cv=3)
+print("Precision score: " + str(round(100*precision['test_score'].mean(), 2)) + "%")
+
+# Передбачення результату для тестової точки даних
+input_data = ['38', 'Private', '215646', 'HS-grad', '9', 'Never-married', 'Handlers-cleaners', 'Not-in-family',
+              'White', 'Male', '0', '0', '40', 'United-States']
+
+# Кодування тестової точки даних
+input_data_encoded = [-1] * len(input_data)
+count = 0
+for i, item in enumerate(input_data):
+    if item.isdigit():
+        input_data_encoded[i] = int(input_data[i])
+    else:
+        arr = np.array(input_data[i], ndmin=1)
+        input_data_encoded[i] = label_encoder[count].transform(arr)[0]
+        count += 1
+
+input_data_encoded = np.array(input_data_encoded).reshape(1, -1)
+
+# Використання класифікатора для кодованої точки даних
+# та виведення результату
+predicted_class = classifier.predict(input_data_encoded)
+print(label_encoder[-1].inverse_transform(predicted_class))
---
+++ LR_2_task_4.py
@@ -0,0 +1,63 @@
+import numpy as np
+import sklearn.metrics
+from pandas import read_csv
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import StratifiedKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+names = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship',
+         'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
+dataset = read_csv('income_data.txt', names=names)
+dataset.dropna(inplace=True)
+
+# Розділення датасету на навчальну та контрольну вибірки
+array = dataset.to_numpy()
+
+label_encoder = []
+X_encoded = np.empty(array.shape)
+
+# Кодуємо символьні атрибути у числові
+for i,item in enumerate(array[0]):
+    if isinstance(item, int|float):
+        X_encoded[:, i] = array[:, i]
+    else:
+        label_encoder.append(preprocessing.LabelEncoder())
+        X_encoded[:, i] = label_encoder[-1].fit_transform(array[:,i])
+
+X = X_encoded[::3, :-1].astype(int)
+y = X_encoded[::3, -1].astype(int)
+
+# Разделение X и y на обучающую и контрольную выборки
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+# Завантажуємо алгоритми моделі
+models = {}
+models['LR'] = LogisticRegression(solver='liblinear', multi_class='ovr')
+models['LDA'] = LinearDiscriminantAnalysis()
+models['KNN'] = KNeighborsClassifier()
+models['CART'] = DecisionTreeClassifier()
+models['NB'] = GaussianNB()
+models['SVM'] = SVC(gamma='auto')
+
+# оцінюємо модель на кожній ітерації
+results = []
+names = []
+
+for name, model in models.items():
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_validate(model, X_train, Y_train, cv=kfold, scoring=['accuracy', 'recall', 'precision', 'f1'])
+    results.append(cv_results)
+    names.append(name)
+    print(f'\n{name}')
+    print(f'Accuracy {cv_results["test_accuracy"].mean():.2f}')
+    print(f'Recall {cv_results["test_recall"].mean():.2f}')
+    print(f'Precision {cv_results["test_precision"].mean():.2f}')
+    print(f'F1 {cv_results["test_f1"].mean():.2f}')
+
